ec5e058b77e52718326ee4ed7d0242bda1f5efb2
AlexandrKurbanov19 <49018108+AlexandrKurbanov19@users.noreply.github.com>
2020-06-24 15:18:25 +0700

    Update sale.js

--- js/sale.js
+++ js/sale.js
@@ -1,1 +1,12 @@
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains("btn")) {
+      ++e.target.parentElement.querySelector("input").value;
+    } else if (e.target.classList.contains("decrease")) {
+      --e.target.parentElement.querySelector("input").value;
+    }
+  })
 
+  var I = document.getElementById('elem');
+  var counts = document.getElementById('count');
+ 
+  
